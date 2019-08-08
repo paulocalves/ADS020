8de3ab28a6f7df2c363eb4ef4f87beabc32a7450
18114290041 <18114290041@OLAB04-D25.academico.iesb.br>
2019-08-07 21:56:41 -0300

    Construindo a atividade 01

---
+++ Atividade_01/src/modelo/persistencia/AlunoBO.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.persistencia;
+
+import modelo.entidades.Aluno;
+
+/**
+ *
+ * @author 18114290041
+ */
+public class AlunoBO implements BO<Aluno>{
+
+    @Override
+    public void validar(Aluno entidade) throws NegocioException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void lista() throws NegocioException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void incluir(Aluno entidade) throws NegocioException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alterar(Aluno entidade) throws NegocioException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void excluir(Aluno entidade) throws NegocioException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void consultar(Aluno entidade) throws NegocioException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
