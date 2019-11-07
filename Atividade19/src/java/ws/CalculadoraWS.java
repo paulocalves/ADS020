a964b1c8429b42391a21e29dd6c8151de90dfa7c
18114290041 <18114290041@OLAB04-D25.academico.iesb.br>
2019-11-06 22:57:00 -0200

    Atividade 19

---
+++ Atividade19/src/java/ws/CalculadoraWS.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author 18114290041
+ */
+@WebService(serviceName = "CalculadoraWS")
+public class CalculadoraWS {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "somarInteiros")
+    public int somar(@WebParam(name = "operadorA") int a, @WebParam(name = "operadorB") int b) {
+        return a + b;
+    }
+
+    @WebMethod(operationName = "subtrairInteiros")
+    public int subtrair(@WebParam(name = "operadorA") int a, @WebParam(name = "operadorB") int b) {
+        return a - b;
+    }
+
+    @WebMethod(operationName = "multiplicarInteiros")
+    public int multiplicar(@WebParam(name = "operadorA") int a, @WebParam(name = "operadorB") int b) {
+        return a * b;
+    }
+
+    @WebMethod(operationName = "dividirInteiros")
+    public int dividir(@WebParam(name = "operadorA") int a, @WebParam(name = "operadorB") int b) {
+        return a / b;
+    }
+
+}
